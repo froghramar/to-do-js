e7d80b81ec29e5bcb521f5eed3f9af9405abc3e7
Feroz Ahmmed <froghramar@gmail.com>
2017-02-14 17:28:21 +0600

    tasklist action addeed

--- model.js
+++ model.js
@@ -1,1 +1,17 @@
-﻿
+﻿function edit(item) {
+    item = item.parentNode;
+    var id = item.getAttribute('data-task-id');
+    alert('edit ' + id);
+}
+
+function del(item) {
+    item = item.parentNode;
+    var id = item.getAttribute('data-task-id');
+    alert('del ' + id);
+}
+
+function check(item) {
+    item = item.parentNode;
+    var id = item.getAttribute('data-task-id');
+    alert('check ' + id);
+}
